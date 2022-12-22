2c653f96a4d3ca43bcc3c61c428ba90da80f1e6c
Kubabob <114347708+Kubabob@users.noreply.github.com>
2022-12-22 13:00:17 +0100

    Update Substancja.py

--- Substancja.py
+++ Substancja.py
@@ -1,49 +1,42 @@
 ile_pomiarow = int(input())
 
-roznice = []
-index_max = []
-max_lokalne = []
+roznice = [0]
+index_max = [0]
 
 
-for pomiar in range(ile_pomiarow):
-    try:
-        wejscie = list(map(int, input().split()))
+wejscie = list(map(int, input().split()))
 
-        if wejscie[1] > wejscie[0]:
-            roznice = [0,1]
-            index_max = [0,1]
-            max_lokalne = [wejscie[0],wejscie[1]]
+max_lokalne = [wejscie[0]]
+
+index = 0
+for liczba in wejscie[1:]:
+  index += 1
+  ile_max = 0
+  for maximum in max_lokalne[::-1]:
+    if liczba > maximum:
+      ile_max += 1
+    else:
+      break
+  if ile_max:
+    roznice.append(index - wejscie.index(max_lokalne[-ile_max]))
+    index_max.append(index)
+    max_lokalne.append(liczba)
+  else:
+    suma_roznicy = 0
+
+    sprawdzanie = False
+
+    if liczba > wejscie[index-1]:
+      sprawdzanie = True
+    else:
+      roznice.append(suma_roznicy)
+
+    if sprawdzanie:
+      for i in range(index-1, wejscie.index(max_lokalne[-1]) -1, -1):
+        if liczba > wejscie[i]:
+          suma_roznicy += 1
         else:
-            roznice = [0,0]
-            index_max = [0]
-            max_lokalne = [wejscie[0]]
+          roznice.append(suma_roznicy)
+    
 
-
-        index = 1
-        for liczba in wejscie[2:]:
-            index += 1
-            ile_max = 0
-            for maximum in max_lokalne[::-1]:
-                if liczba > maximum:
-                    ile_max += 1
-                else:
-                    break
-            if ile_max:
-                roznice.append(index - wejscie.index(max_lokalne[-ile_max]))
-                index_max.append(index)
-                max_lokalne.append(liczba)
-            else:
-                suma_roznicy = 0
-                for i in range(index-1, wejscie.index(max_lokalne[-1])-1, -1):
-
-                    if liczba > wejscie[i]:
-                        suma_roznicy += 1
-                    else:
-                        roznice.append(suma_roznicy)
-
-
-    except:
-        print(*roznice)
-        break
-
-
+print(*roznice)
